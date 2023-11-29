7e36462fbed35cfb06725a3b3190c7ca5b228cde
Hmmlov <miguelcondegarcia26@gmail.com>
2023-11-29 15:12:13 -0500

    first commit

---
+++ date.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+fecha_actual = date.today()
+
+print(fecha_actual)
+
+print("Today's date is: " + str(date.today()))
---
+++ number-input.py
@@ -0,0 +1,6 @@
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(int(first_number) + int(second_number))
+
+print("1" + 2 )
